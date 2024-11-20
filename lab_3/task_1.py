8f6e003819c57820f2749ddee85746684012af3b
Vertushkins <152332518+Vertushkins@users.noreply.github.com>
2024-11-20 23:49:32 +0300

    Update task_1.py

--- lab_3/task_1.py
+++ lab_3/task_1.py
@@ -1,15 +1,15 @@
 # TODO Напишите функцию для поиска индекса товара
 
-def find_first_index(list_, item):
-    for i in range(len(list_)):  # Перебираем все индексы списка
-        if list_[i] == item:  # Если находим нужный элемент, то возвращаем его индекс
-            return i
+def find_first_index(list_, find_item):
+    for index, item in enumerate(list_):  # Перебираем все индексы и элементы списка
+        if item == find_item:  # Если находим нужный элемент, то возвращаем его индекс
+            return index
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_first_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    index_item = find_first_index(items_list, find_item)  # TODO Вызовите функцию, чтобы получить индекс товара
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
